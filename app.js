265c854112b40152f54edea54d8bcf99405d5616
tariq <tariq@newvision.com>
2018-01-15 20:39:03 +0200

    NameCalculator

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 (function () {
-  angular.module("myFirstApp",[]);
+'use strict';
+  angular.module('NameCalculator',[]).
+  controller('NameCalculatorController',function ($scope) {
+    $scope.name = "";
+    $scope.totalValue = 0;
+    $scope.DisplayTotalValue = function () {
+      var totalValue = GetTotalvalue($scope.name);
+      $scope.totalValue = totalValue;
+    };
+  })
+  function GetTotalvalue(name) {
+    var value = 0;
+    for(var i =0;i<name.length ;i++){
+      value +=name.charCodeAt(i);
+    }
+    return value;
+  }
 })();
